3371bbc01faa811809bd6c379dad73a2b813114b
João Vitor <dreamteamads2021@gmail.com>
2022-05-23 20:03:37 -0300

    a

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ import app from './src/app.js';
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log(`Servidor escutando em http://192.168.43.117:${port}`);
+    console.log(`Servidor escutando em http://192.168.159.117:${port}`);
 });
